18dd262a68b8f4d9da0badc59215b160b22f9010
Serhat YAREN <64776783+SerhatYaren09@users.noreply.github.com>
2021-08-27 22:15:06 +0300

    Update post.php

--- post.php
+++ post.php
@@ -22,4 +22,104 @@
  * warning=sarı, normal=arkaplan yok)
  * - `getPostDetails` fonksiyonu tetiklenerek ilgili içeriğin çıktısı gösterilmeli.
  */
+   require_once("functions.php"); //Dosyayı birkez getirmek istediğimizden ve parametre olarak verdiğimiz dosya yolu bulunamazsa error verip çalışmasını durduğu için kullanmak istedim
 
+if(!isset($posts))
+{ 
+
+    $randomPostCount = getRandomPostCount(1, 100); // 1-100 arasında rasgele sayı döndürür
+    $posts = getLatestPosts($randomPostCount); // rasgele post sayı değerine göre son postları dizi olarak getirir
+
+
+    foreach($posts as $id => $post){ 
+        $id_array[] = $id; // $posts dizisindeki tüm id değerlerini yeni tanımladığımız değişkene ekler.  
+    }
+
+    for($i=1; $i<1000; $i++) { 
+        if(in_array($i,$id_array)) {  // $i sayısı $id_array dizisinde tanımlı ise,
+           continue; // devam et yani bir sonraki değere gider
+        } else { 
+            $temporal = $i; // Geçici değişkene, $i değişkenini atar
+            break; // döngüden çık.
+        }
+    }
+
+    $id = $temporal; // $id değişkenine geçici değişken değerini atar
+
+    $myPost = [];
+    $myPost[$id] = [ 
+        "title" => "Hello PHP",
+        "type" => "Type Declaration" ];
+
+    $posts[] = $myPost; 
+
+    foreach($myPost as $id => $post) { 
+// div $type değerine göre arkaplan rengi oluşturma
+        if($post['type'] =="urgent") { 
+              echo "<div style = 'background-color:red' >";
+        } 
+        elseif($post['type'] =="warning") {
+              echo "<div style = 'background-color:yellow' >";
+        } 
+        else {
+              echo "<div>";
+        }
+// Verilen parametrelere göre ekrana detayları getirme
+        getPostDetails($id, $post['title']); 
+             echo "</div>";
+    }
+}
+// Eğer yukarıda $posts isimli değişken tanımlandıysa aşağıdaki kodları çalıştırır
+else { 
+
+        if(!isset($id)){
+
+        foreach($posts as $id => $post){ 
+             $id_array[] = $id; // $posts dizisindeki tüm id değerlerini yeni tanımladığımız değişkene ekler.
+            }
+
+        for($i=1; $i<1000; $i++) {  
+             if(in_array($i,$id_array)) { // $i sayısı $id_array dizisinde tanımlı ise
+                 continue; // devam et yani bir sonraki değere gider 
+            }    
+        else { 
+         $temporal = $i; //Geçici değişkene, $i değişkenini atar
+                 break; // döngüden çıkar. 
+            }
+        
+        }
+
+  $id = $temporal; //$id değişkenine geçici değişken değerini atar
+            }
+  $post[$id] = [
+  'title' => $post['title'],
+  'type' => $post['type']
+           ];
+     // Eğer posts array içerisindeki geçerli dizi değeri içerisinde $title değeri yoksa,
+        if(!isset($post['title'])){ 
+             $post['title'] = "Added Title"; // mevcut post dizisine "title" değeri ekle
+        }
+         // Eğer posts array içerisindeki geçerli dizi değeri içerisinde $type değeri yoksa,
+        if(!isset($post['type'])){ 
+             $post['type'] = "warning"; // mevcut post dizisine "type" değeri ekle
+        }
+
+
+    // Eğer posts arrayi içerisindeki geçerli dizi değeri içerisinde : $id değeri tanımlı değilse; 
+    if($post['type'] == "urgent") 
+      { 
+           echo "<div style = 'background-color:red' >"; // $post dizisinde "type" değerine göre; arkaplan rengi almalıdır
+       }
+    elseif($post['type'] == "warning") 
+    { 
+           echo "<div style = 'background-color:yellow' >"; //$post dizisinde "type" değerine göre; arkaplan rengi alma
+    } 
+    else {
+           echo "<div>";
+    }
+     // getPostDetails` fonksiyonu tetiklenerek ilgili içeriğin çıktısı gösterilmesi
+    getPostDetails($id, $post['title']);
+           echo "</div>";
+
+}
+?>
